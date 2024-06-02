db652498aae543ff2f37232fc54821e4916fa075
rionando <rionandosoeksin@gmail.com>
2024-06-02 19:32:37 +0800

    adding topics to the model

--- base/admin.py
+++ base/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import News, UserProfile
+from .models import News, UserProfile, Topics
 
 # Register your models here.
 admin.site.register(News)
 admin.site.register(UserProfile)
+admin.site.register(Topics)
